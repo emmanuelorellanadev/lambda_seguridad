80787e5844cf4f3704a51c9434cac92c95a4dd16
emmanuelorellanadev <80858597+emmanuelorellanadev@users.noreply.github.com>
2025-02-21 19:08:13 -0600

    Delete lambdaConnection.session.sql

--- lambdaConnection.session.sql
+++
@@ -1,9 +0,0 @@
--- delete
-
--- SET FOREIGN_KEY_CHECKS = 0;
--- DROP TABLE Reservations;
--- DROP TABLE ReservationDetails;
--- DROP TABLE ReservationStates;
--- SET FOREIGN_KEY_CHECKS = 1;
-
--- DESCRIBE Rooms;
